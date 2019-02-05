b3e58c39c96e171aece94de75fe701a2858d09ed
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-02-05 22:19:51 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,6 +1,37 @@
+function getIt(){
+  $('p').on("eventname", function(){
+      return alert("Hey!");//action you want taken
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $("img").addClass("tasty")
+  });
+}
+
+function pressIt(){
+$(document).on('keydown', function(key) {
+if(key.which == “g”){
+alert('G was pressed');
+}
+});
+}
+
+function submitIt(){
+$("form").on("submit", function() {
+    alert('your form is going to be submitted now');
+    return;
+});
+}
 //define functions here
 
 $(document).ready(function(){
+
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 // call functions here
 
