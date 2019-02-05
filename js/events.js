7630869164bcc70000e70fe62de6fd44bcce166f
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-02-05 22:24:11 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,6 +1,36 @@
+function getIt(){
+  $('p').on('click', function(){
+      alert("Hey!");//action you want taken
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  });
+}
+
+function pressIt(){
+$(document).on('keydown', function(key) {
+if(key.which == 71){
+alert('G was pressed');
+}
+});
+}
+
+function submitIt(){
+$('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+    });
+}
 //define functions here
 
 $(document).ready(function(){
+
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 // call functions here
 
